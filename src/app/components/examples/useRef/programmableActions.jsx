4be64fee91fa5acd4ac7f25bfeb7484957e01958
Artem Musin <atm759@mail.ru>
2022-03-18 16:56:27 +0300

    useRef learned

--- src/app/components/examples/useRef/programmableActions.jsx
+++ src/app/components/examples/useRef/programmableActions.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 import CardWrapper from "../../common/Card";
+import Divider from '../../common/divider'
 import SmallTitle from "../../common/typografy/smallTitle";
 const ProgrammableActionsExample = () => {
+
+    const inputRef = useRef()
+
+    const handleClick = () => {
+        inputRef.current.focus()
+        console.log(inputRef.current)
+    }
+    const handleClickWidth = () => {
+        inputRef.current.style.width = '180px'
+    }
     return (
         <CardWrapper>
             <SmallTitle className="card-title">
                 Программируемые действия и свойства
             </SmallTitle>
+            <Divider />
+            <label htmlFor="email" className="form-label">Email</label>
+            <input ref = {inputRef} type="email" className="form-control" id="email"/>
+            <button className="btn btn-primary" onClick={handleClick}>focus input</button>
+            <button className="btn btn-secondary" onClick={handleClickWidth}>Изменить ширину объекта</button>
         </CardWrapper>
     );
 };
--- src/app/components/examples/useRef/renderCountMyVersion.jsx
+++ src/app/components/examples/useRef/renderCountMyVersion.jsx
@@ -1,24 +1,17 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import CardWrapper from "../../common/Card";
-import Divider from "../../common/divider";
 import SmallTitle from "../../common/typografy/smallTitle";
 const RenderCountExample = () => {
-    const renderCount = useRef(0);
-    const [otherState, setOtherState] = useState(false)
-
+    const [renderCount, setRenderCount] = useState(0)
     const toggleOtherState = () => {
-        setOtherState(!otherState)
+        setRenderCount(prevState => prevState+1)
     }
-
-    useEffect(() => {
-        renderCount.current++
-    }, )
 
     return (
         <CardWrapper>
             <SmallTitle>Подсчет количества рендеров</SmallTitle>
-            <Divider/>
-            <p>render count: {renderCount.current}</p>
+
+            <p>render count: {renderCount}</p>
             <button className="btn btn-primary" onClick={toggleOtherState}>Toggle other state</button>
         </CardWrapper>
     );
