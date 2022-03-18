a1bbe40babe2f29cf3fbbdf57f9133884db93fef
Artem Musin <atm759@mail.ru>
2022-03-18 18:24:42 +0300

    доделать дома useMemo (7 минута)

--- src/app/components/exercises/useRefExercise.jsx
+++ src/app/components/exercises/useRefExercise.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import CollapseWrapper from "../common/collapse";
+import Divider from "../common/divider";
 const UseRefExercise = () => {
+    const smallRef = useRef()
+
+    const changeSmall = () => {
+        smallRef.current.textContent = 'text'
+        smallRef.current.style.width = '150px'
+        smallRef.current.style.height = '80px'
+    }
+
     return (
         <CollapseWrapper title="Упражнение">
+            <Divider/>
             <p className="mt-3">
                 У вас есть блок, у которого заданы ширина и высота. Добавьте
                 кнопку, при нажатии которой изменятся следующие свойства:
-            </p>
+            </p>            
             <ul>
                 <li>Изменится содержимое блока на &quot;text&quot;</li>
                 <li>высота и ширина станут равны 150 и 80 соответственно</li>
@@ -18,9 +28,11 @@ const UseRefExercise = () => {
                     width: 60,
                     color: "white"
                 }}
+                ref={smallRef}
             >
                 <small>Блок</small>
-            </div>
+                <button className="btn btn-secondary" onClick={changeSmall}>Изменить блок</button>
+            </div>            
         </CollapseWrapper>
     );
 };
